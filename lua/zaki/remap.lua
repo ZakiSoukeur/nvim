66a814abc418269f03ac31ca9b1b2c3315ebaa2c
ZakiSoukeur <zakaria@lazdek.com>
2025-07-08 01:14:18 +0000

    yanks

--- lua/zaki/remap.lua
+++ lua/zaki/remap.lua
@@ -5,3 +5,7 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
 vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
 vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
