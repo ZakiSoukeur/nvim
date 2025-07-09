a44d130bcf6419a1b49045446dd3b04c2bef49f9
ZakiSoukeur <zakaria@lazdek.com>
2025-07-09 14:48:12 +0000

    fix:vertiacl tabs

--- lua/zaki/packer.lua
+++ lua/zaki/packer.lua
@@ -43,4 +43,11 @@ return require('packer').startup(function(use)
             vim.cmd("colorscheme catppuccin")
         end
     }
+    use({
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!:).
+        run = "make install_jsregexp"
+    })
 end)
--- lua/zaki/remap.lua
+++ lua/zaki/remap.lua
@@ -13,13 +13,8 @@ vim.keymap.set('n', '<leader>Y', '"+Y', { noremap = true, silent = true })
 -- Use <leader>p to paste from system clipboard
 vim.keymap.set({ 'n', 'v' }, '<leader>p', '"+p', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>P', '"+P', { noremap = true, silent = true })
+-- CTRL+T to split the current window vertically
+vim.keymap.set('n', '<C-t>', ':vsplit<CR>', { noremap = true, silent = true })
 
--- CTRL+T to vsplit current buffer in a new tab
-vim.keymap.set('n', '<C-t>', function()
-    vim.cmd('vsplit')
-end, { noremap = true, silent = true })
-
--- x to close the current tab (only in normal mode)
--- WARNING: If you bind 'x' alone, it will override Vim's 'x' (delete char)
--- Better: use <leader>x or another key
-vim.keymap.set('n', '<leader>x', ':tabclose<CR>', { noremap = true, silent = true })
+-- Use <leader>x to close the current split window
+vim.keymap.set('n', '<C-x>', ':close<CR>', { noremap = true, silent = true })
